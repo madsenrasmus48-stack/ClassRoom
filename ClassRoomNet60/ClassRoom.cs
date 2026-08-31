23dd72ab09f7aa5dbf5effa65270545178eca9bd
Rasmus Madsen <ram006@edu.zealand.dk>
2026-08-31 11:32:13 +0200

    Oprettede ClassRoom Klasse, (task2)

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60;
+
+public class ClassRoom
+{
+    public string ClassName { get; }
+    public List<Student> StudentList { get; set; }
+    public DateTime SemesterStart { get; }
+
+    public ClassRoom(string className, DateTime semesterStart)
+    {
+        StudentList = new List<Student>();
+        ClassName = className;
+        SemesterStart = semesterStart;
+    }
+
+}
